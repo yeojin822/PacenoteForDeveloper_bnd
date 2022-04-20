75596bc24a97dc7d88a250ec94a1185e2ebd4b39
yeojin <yeojin822>
2022-04-20 23:21:00 +0900

    feat : initial commit

---
+++ src/main/java/com/example/portfoliopagebuilder_bnd/PortfolioPageBuilderBndApplication.java
@@ -0,0 +1,13 @@
+package com.example.portfoliopagebuilder_bnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioPageBuilderBndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioPageBuilderBndApplication.class, args);
+    }
+
+}
