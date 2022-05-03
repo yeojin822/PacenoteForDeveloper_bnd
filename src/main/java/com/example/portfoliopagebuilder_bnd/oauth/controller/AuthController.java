1ba1769251addf43f676ae284be7cf09260bab06
yeojin822 <xlzt2002@gmail.com>
2022-05-03 11:23:46 +0900

    fix : controller 주소 수정

--- src/main/java/com/example/portfoliopagebuilder_bnd/oauth/controller/AuthController.java
+++ src/main/java/com/example/portfoliopagebuilder_bnd/oauth/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.example.portfoliopagebuilder_bnd.oauth.controller;
 import com.example.portfoliopagebuilder_bnd.oauth.dto.PrincipalDetails;
 import com.example.portfoliopagebuilder_bnd.oauth.model.User;
 import com.example.portfoliopagebuilder_bnd.oauth.repository.UserRepository;
+import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,15 +12,15 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 
-@Controller
 @RequiredArgsConstructor
 @Slf4j
+@RestController
+@Api(value = "AuthController")
+@RequestMapping("/api/v1/auth")
 public class AuthController {
 
 	private final UserRepository userRepository;
@@ -32,9 +33,9 @@ public class AuthController {
 	// 그래서 PrincipalDetails에 UserDetails, OAuth2User 모두 구현해야 함
 
 	// Authentication 객체 안에 인증된 유저 정보 가져오기
-	@GetMapping("/test/login")
-	public @ResponseBody String testLogin(Authentication authentication, @AuthenticationPrincipal PrincipalDetails userDetails){
-		log.info("==== /test/login =====");
+	@GetMapping("/login")
+	public String testLogin(Authentication authentication, @AuthenticationPrincipal PrincipalDetails userDetails){
+		log.info("==== /login =====");
 		PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
 		log.info("authentication : " + principalDetails.getUser());
 		log.info("userDetails : " + userDetails.getUser());
@@ -43,9 +44,9 @@ public class AuthController {
 	}
 
 	// Authentication 객체 안에 oauth 인증된 유저 정보 가져오기
-	@GetMapping("/test/oauth/login")
-	public @ResponseBody String testOAuthLogin(Authentication authentication, @AuthenticationPrincipal OAuth2User oAuth2User){
-		log.info("==== /test/oauth/login =====");
+	@GetMapping("/oauth/login")
+	public String testOAuthLogin(Authentication authentication, @AuthenticationPrincipal OAuth2User oAuth2User){
+		log.info("==== /oauth/login =====");
 		OAuth2User oAuth2UserObj = (OAuth2User) authentication.getPrincipal();
 		log.info("authentication : " + oAuth2UserObj.getAttributes());
 		log.info("oAuth2User : " + oAuth2User.getAttributes());
@@ -56,18 +57,18 @@ public class AuthController {
 	// loadUser, loadUserByUsername에서 각각 로그인 유저 객체를 Authentication에 저장해줌
 	// @AuthenticationPrincipal 어노테이션으로 Authentication에 저장되어 있는 일반, OAuth 로그인 유저 객체 모두 받아올 수 있음
 	@GetMapping("/user")
-	public @ResponseBody String user(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+	public String user(@AuthenticationPrincipal PrincipalDetails principalDetails) {
 		log.info("User : " + principalDetails.getUser());
 		return "user";
 	}
 
 	@GetMapping("/admin")
-	public @ResponseBody String admin(){
+	public String admin(){
 		return "admin";
 	}
 
 	@GetMapping("/manager")
-	public @ResponseBody String manager(){
+	public String manager(){
 		return "manager";
 	}
 
@@ -92,13 +93,13 @@ public class AuthController {
 
 	@Secured("ROLE_ADMIN")
 	@GetMapping("/data")
-	public @ResponseBody String data(){
+	public String data(){
 		return "data only admin";
 	}
 
 	@PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
 	@GetMapping("/users")
-	public @ResponseBody String userList(){
+	public String userList(){
 		return "user list only manager, admin";
 	}
 }
