035e2f669ac7a9dfb461996f3091a8475529bcd0
yeojin822 <xlzt2002@gmail.com>
2022-05-09 17:11:13 +0900

    feat : 예외처리 추가

---
+++ src/main/java/com/example/portfoliopagebuilder_bnd/oauth/exception/OAuth2ProcessingException.java
@@ -0,0 +1,16 @@
+package com.example.portfoliopagebuilder_bnd.oauth.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+
+@Slf4j
+public class OAuth2ProcessingException extends AuthenticationException {
+
+    public OAuth2ProcessingException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+    public OAuth2ProcessingException(String msg) {
+        super(msg);
+    }
+}
