06f8540077ac61dac12816f99696b9f6d66be241
JIWON KIM <kkjim1324@naver.com>
2022-07-03 21:57:44 +0900

    feat : swagger version 변경 완료

--- src/main/java/com/example/portfoliopagebuilder_bnd/test/SwaggerTestController.java
+++ src/main/java/com/example/portfoliopagebuilder_bnd/test/SwaggerTestController.java
@@ -1,21 +1,22 @@
-//package com.example.portfoliopagebuilder_bnd.test;
-//
-//import io.swagger.annotations.ApiOperation;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
-//
-//
-//import java.time.LocalDate;
-//import java.time.LocalDateTime;
-//
-//@Controller
-//@RequestMapping("/test/swagger")
-//public class SwaggerTestController {
-//
-//    @GetMapping("/time")
-//    @ApiOperation(value = "test")
-//    @ResponseBody
-//    public LocalDateTime Hello(){
-//        return LocalDateTime.now();
-//    }
-//}
+package com.example.portfoliopagebuilder_bnd.test;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Controller
+@OpenAPIDefinition(info = @Info(title = "테스트", description = "테스트URL"))
+@RequestMapping("/test/swagger")
+public class SwaggerTestController {
+
+    @GetMapping("/time")
+    @ResponseBody
+    public LocalDateTime Hello(){
+        return LocalDateTime.now();
+    }
+}
