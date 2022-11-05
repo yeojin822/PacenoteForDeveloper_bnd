1f9491d917abd24dbfd7b40252aece3d74872afe
yeojin822 <xlzt2002@gmail.com>
2022-11-05 21:14:28 +0900

    fix : swagger 정보 수정

--- src/main/java/com/example/portfoliopagebuilder_bnd/builder/service/BuilderService.java
+++ src/main/java/com/example/portfoliopagebuilder_bnd/builder/service/BuilderService.java
@@ -2,10 +2,11 @@ package com.example.portfoliopagebuilder_bnd.builder.service;
 
 import com.example.portfoliopagebuilder_bnd.builder.dto.Builder;
 import com.example.portfoliopagebuilder_bnd.builder.dto.BuilderType;
+import com.example.portfoliopagebuilder_bnd.builder.dto.DeleteInfo;
 import org.springframework.http.ResponseEntity;
 
 public interface BuilderService {
     ResponseEntity<?> detail(String id) throws Exception;
     ResponseEntity<?> save(Builder param) throws Exception;
-    ResponseEntity<?> delete(BuilderType param);
+    ResponseEntity<?> delete(DeleteInfo param);
 }
